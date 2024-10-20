0dfd2ecbf44b9be7c7a247a2517c6f5d3148477f
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-10-20 18:26:40 +0200

    fibonachi

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,58 +1,21 @@
-import Dragons.Dragons;
-import Dragons.typeD;
 import java.util.Arrays;
 import java.util.Scanner;
-//import Dragons.Mal;
-
 
 public class main {
 
     public static void main(String[] arg) {
-
-        Scanner me = new Scanner(System.in);
-        System.out.println("Давай створимо Дракона");
-        System.out.println("Для початку потрібно обрати вид Дракона");
-        System.out.println(Arrays.toString(typeD.values()));
-        typeD TD = typeD.valueOf(me.nextLine());
-        System.out.println("Як назвем твого Дракона?");
-        String name = me.nextLine();
-        System.out.println("Чи старенький твій Дракончик?");
-        int age = me.nextInt();
-        System.out.println("Скільки важить твій Дракон?");
-        int weigth = me.nextInt();
-//        Scanner cs = new Scanner(System.in);
-        System.out.println("а що вміє твій Дракон? flame or fly?");
-        String sp = me.next();
-
-        Dragons newDragons = new Dragons(name, age, weigth, TD);
-//        Dragons newDragons = new Dragons("Osiris", 154, 578, typeD.Amphithere);
-         System.out.print(newDragons);
-
-
-        boolean o ;
-        o = sp.equals("flame");
-
-        if (o == true) {
-            String emoji = "\uD83D\uDC09\uD83D\uDD25";
-            System.out.println(emoji);
+int f1 = 0;
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int i = 1;
+        int [] fib;
+        fib = new int [num];
+        fib [0] = 0;
+        fib [1] = 1;
+        while (i < num -1){
+            fib[i+1]= fib[i-1]+ fib[i];
+            i++;
         }
-            else {
-                String emoji1 = "\uD83D\uDC32";
-                System.out.println(emoji1);
-            }
-        }
+System.out.println(Arrays.toString(fib));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
